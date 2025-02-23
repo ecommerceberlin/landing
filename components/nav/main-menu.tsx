3f2da735a98f15497964e89c4f3203ebbaeb46ed
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-02-23 22:49:53 +0100

    up

--- components/nav/main-menu.tsx
+++ components/nav/main-menu.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react';
 import Link from 'next/link';
-
 import { cn } from '@/lib/utils';
 // import { Logo } from "@/components/icons"
 import {
@@ -14,48 +13,18 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
+import { menuExhibitors } from '@/settings/menu';
 
 export interface MainMenuProps {
   className?: string;
 }
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Alert Dialog',
-    href: '/docs/primitives/alert-dialog',
-    description:
-      'A modal dialog that interrupts the user with important content and expects a response.',
-  },
-  {
-    title: 'Hover Card',
-    href: '/docs/primitives/hover-card',
-    description:
-      'For sighted users to preview content available behind a link.',
-  },
-  {
-    title: 'Progress',
-    href: '/docs/primitives/progress',
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'Scroll-area',
-    href: '/docs/primitives/scroll-area',
-    description: 'Visually or semantically separates content.',
-  },
-  {
-    title: 'Tabs',
-    href: '/docs/primitives/tabs',
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Tooltip',
-    href: '/docs/primitives/tooltip',
-    description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-  },
-];
+export interface MenuOption {
+  title: string;
+  href: string;
+  description: string;
+}
+
 
 export function MainMenu() {
   return (
@@ -99,7 +68,7 @@ export function MainMenu() {
           <NavigationMenuTrigger>Exhibitors</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
+              {menuExhibitors.map((component) => (
                 <ListItem
                   key={component.title}
                   title={component.title}
---
+++ settings/menu.ts
@@ -0,0 +1,41 @@
+
+import { MenuOption } from "@/components/nav/main-menu";
+
+
+export const menuExhibitors: MenuOption[] = [
+    {
+      title: 'Lorem ipsum dolor sit amet',
+      href: '/exhibitors',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet',
+      href: '/exhibitors',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet',
+      href: '/exhibitors',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet',
+      href: '/exhibitors',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet',
+      href: '/exhibitors',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet',
+      href: '/exhibitors',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+    },
+  ];
