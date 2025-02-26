bf6bf2aacf818711a6b1d97c0d368757989c9094
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-02-26 19:06:00 +0100

    revert to use std Image component

--- components/sections/brands.tsx
+++ components/sections/brands.tsx
@@ -1,14 +1,13 @@
 "use client"
 
 import * as React from 'react';
-import { CldImage } from 'next-cloudinary';
 import { cn } from '@/lib/utils';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import useSWR from 'swr';
 import { fetcher } from '@/lib/fetcher';
 import { ExhibitorDTO } from '@/db/participant/transform-to-exhibitor';
 import { brands } from '@/settings/brands';
-
+import Image from 'next/image';
 
 export interface BrandsProps {
   logotypes?: string[];
@@ -25,15 +24,12 @@ export function Brands({logotypes = brands}: BrandsProps) {
               key={idx}
               className="shrink-0 w-[200px] h-[100px] flex items-center justify-center mr-8 last:mr-0 "
             >              
-                <CldImage
+                <Image
                   src={logotype}
                   alt={logotype}
                   width={200}
                   height={100}
-                  className="object-contain max-w-[200px] max-h-[80px] p-2"
-                  grayscale={true}
-                  removeBackground={true}
-                  format="png"
+                  className="grayscale object-contain max-w-[200px] max-h-[80px] p-2"
                 />
              
             </figure>
