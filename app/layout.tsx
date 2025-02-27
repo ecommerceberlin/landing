b7ae406d7572abbe6f43da49c56b439cb945747c
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-02-27 17:38:53 +0100

    added chatlio

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,20 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { DM_Sans } from 'next/font/google';
 import { metadata as metadataSettings } from '@/settings/metadata';
-
+import Script from 'next/script';
+import { chatlioWidgetId } from '@/settings/app_rules';
 import { RefererTracker } from '@/components/forms/referer-tracker';
+
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'chatlio-widget': any;
+    }
+  }
+}
+
+
 
 const dmSans = DM_Sans({ subsets: ['latin'] });
 
@@ -11,6 +23,7 @@ export const metadata: Metadata = {
   title: metadataSettings.title,
   description: metadataSettings.description,
 };
+
 
 export default async function RootLayout({
   children,
@@ -23,10 +36,19 @@ export default async function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+       
       </head>
 
-      <body>{children}</body>
+      <body>{children}
+
+      <Script src="https://js.chatlio.com/widget.js" strategy="lazyOnload" />
+      <chatlio-widget widgetid={chatlioWidgetId} disable-favicon-badge></chatlio-widget>
+
+      </body>
       <RefererTracker />
     </html>
   );
 }
+
+
+
--- settings/app_rules.ts
+++ settings/app_rules.ts
@@ -1,1 +1,3 @@
 export const eventId = 104;
+
+export const chatlioWidgetId = "027e5aa4-7949-4be7-63d5-515e44e16add";
