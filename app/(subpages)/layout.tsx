bfac9baff989058a2c69d81ca036078f1a11c522
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-02-27 17:39:08 +0100

    added footer data

--- app/(subpages)/layout.tsx
+++ app/(subpages)/layout.tsx
@@ -1,9 +1,9 @@
 import { Header } from "@/components/nav/header";
 import { NowPlaying } from "@/components/nav/now-playing";
-import { SectionTitle } from "@/components/text/section-title";
 import { GrayBox } from "@/components/containers/graybox";
 import { Footer } from "@/components/footer";
 import { nowPlaying } from "@/settings/now-playing";
+import { footer } from "@/settings/footer";
 
 export default function SubpageLayout({
   children,
@@ -18,29 +18,19 @@ export default function SubpageLayout({
 
 
   return (
-
     <div className="min-h-screen w-[100dvw] max-w-[100dvw]">
-    
     <header className="w-full">
-        <NowPlaying {...nowPlaying} />
-        <Header />
+    <NowPlaying {...nowPlaying} />
+    <Header />
     </header>
-
     <main className="m-0 px-5 py-10 w-full max-w-[100dvw]">
-      <GrayBox>
-        <div className="text-left text-[3rem] md:text-[4rem] xl:text-[5rem] font-extralight">{title}</div>
-
-        {children}
-
-      </GrayBox>
-    
-       
-
+    <GrayBox>
+    <div className="text-left text-[3rem] md:text-[4rem] xl:text-[5rem] font-extralight">{title}</div>
+    {children}
+    </GrayBox>
     </main>
-    
     {related}
-    
-    <Footer />
+    <Footer {...footer} />
     </div>
   )
 
