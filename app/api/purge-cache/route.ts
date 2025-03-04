6d457c927b6ed6efc375f3154a4f9d82fd3ef20b
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-03-04 09:42:07 +0100

    Update route.ts

--- app/api/purge-cache/route.ts
+++ app/api/purge-cache/route.ts
@@ -2,9 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag, revalidatePath } from "next/cache";
 
 export async function GET(request: NextRequest) {
-    
+
     const tag = request.nextUrl.searchParams.get('tag')
     const path = request.nextUrl.searchParams.get('path')
+
+    if(!tag && !path) {
+        return NextResponse.json({ message: 'No tag or path provided' }, { status: 400 })
+    }
 
     if (tag) {
         revalidateTag(tag)
