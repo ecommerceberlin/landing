4faacc78cf8ef976494014e757de5872673c9aa3
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-03-05 12:19:36 +0100

    form loading component

--- app/(subpages)/visit/page.tsx
+++ app/(subpages)/visit/page.tsx
@@ -2,10 +2,14 @@ import { Suspense } from "react"
 import { SmartForm } from "@/components/forms/smart-form"
 import { visitorRegistrationSchema } from "@/settings/visitorRegistrationSchema"
 import { Success } from "@/components/forms/success"
+import { SmartFormLoading } from "@/components/forms/smart-form-loading"
+
+export const dynamic = 'force-static'
+
 
 export default function VisitorRegistrationPage() {
   return (<div className="md:ml-[25vw]">
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<SmartFormLoading schema={visitorRegistrationSchema} />}>
     <SmartForm schema={visitorRegistrationSchema} onSuccess={<Success />} />
     </Suspense></div>)
 }
