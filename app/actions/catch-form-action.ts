f003ad1aedbf418203768377623ba8b8cc3a3dd8
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-03-07 12:37:12 +0100

    added fire and forget webhook for slack notifications

--- app/actions/catch-form-action.ts
+++ app/actions/catch-form-action.ts
@@ -1,13 +1,23 @@
 "use server"
 
-import { requestACallSchema } from "@/settings/schemas";
+import { requestACallSchema, newsletterSchema } from "@/settings/schemas";
 import { z } from "zod";
 import { getReferer } from "@/app/actions/get-referer";
 import { catchFormDataToNewsletter } from "@/db/newsletter/catch-form-data-to-newsletter";
-
+import { fireAndForget } from "@/lib/fire-and-forget";
+import { mapContextToSlackWebhook } from "@/settings/notifications";
+import { getWebhookByName } from "@/db/notifications/get-webhooks";
+import { formatObject } from "@/lib/utils";
 
 export async function catchFormAction(context: string, formData: FormData) {
-    const data = requestACallSchema.parse(formData);
+
+    let schema: z.ZodSchema<any> = requestACallSchema
+
+    if(context.startsWith('newsletter')){
+        schema = newsletterSchema
+    }
+
+    const data = schema.parse(formData);
     const referer = await getReferer();
 
     const newsletter = await catchFormDataToNewsletter({
@@ -16,7 +26,16 @@ export async function catchFormAction(context: string, formData: FormData) {
         organizer_id: 5
     })
 
-    console.log({newsletter})
+    // Create a new object without the accept property
+    const { accept, ...dataWithoutAccept } = data;
+
+    const webhook = await(getWebhookByName(mapContextToSlackWebhook(context)))
+
+    if (webhook?.webhook) {
+        fireAndForget(webhook.webhook, {
+            text: formatObject({context, ...dataWithoutAccept, referer })
+        })
+    }
 
     return {success: true, id: newsletter?.id}
 }
