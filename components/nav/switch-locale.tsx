329e905421fa6162a16e555c583b0e39d7981951
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-03-11 12:32:03 +0100

    added pathname to lang switcher

--- components/nav/switch-locale.tsx
+++ components/nav/switch-locale.tsx
@@ -1,21 +1,23 @@
 import {NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle} from '@/components/ui/navigation-menu'
 import { cn } from '@/lib/utils';
 import { resolveAltLocale } from '@/settings/locale';
+import { usePathname } from 'next/navigation';
 
 export function LocaleSwitcher({className}: {className?: string}) {
 
   const altLocale = resolveAltLocale();
+  const pathname = usePathname();
 
   return (
     <NavigationMenuItem className="h-full">
-        <NavigationMenuLink asChild>
-        <a 
-        href={altLocale.href} 
-        className={cn(navigationMenuTriggerStyle(), className)}
-        >
-        {altLocale.locale}
-        </a>
-        </NavigationMenuLink>
-        </NavigationMenuItem>
+    <NavigationMenuLink asChild>
+    <a 
+    href={`${altLocale.href}${pathname}`} 
+    className={cn(navigationMenuTriggerStyle(), className)}
+    >
+    {altLocale.locale}
+    </a>
+    </NavigationMenuLink>
+    </NavigationMenuItem>
   );
 }
