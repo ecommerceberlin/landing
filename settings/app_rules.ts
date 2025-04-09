8fa0c6f2c0f45a6a3d4922e0ab94e9df476f1d48
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-09 19:02:44 +0200

    old GTM id

--- settings/app_rules.ts
+++ settings/app_rules.ts
@@ -1,6 +1,6 @@
 export const eventId = 104;
-export const organizerId = 5;
 
+export const organizerId = 5;
 
 export const chatlioWidgetId = "027e5aa4-7949-4be7-63d5-515e44e16add";
 
@@ -10,4 +10,6 @@ export const basePriceCurrency = "EUR";
 
 export const pricesNetGross = "net";
 
-export const GTM_ID = "GTM-P3VS4G8P" //"GTM-5RJC4J"
+export const GTM_ID = "GTM-5RJC4J" //"GTM-P3VS4G8P"
+
+export const baseUrl = process.env.NODE_ENV === "production" ? `${process.env.NEXT_PUBLIC_BASE_URL}`: "https://legible-key-frog.ngrok-free.app"
