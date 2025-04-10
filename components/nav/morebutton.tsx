e2e390a1c26150bb05e90240357d81e69435cf1a
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-10 10:33:12 +0200

    Update morebutton.tsx

--- components/nav/morebutton.tsx
+++ components/nav/morebutton.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import { Button} from "@/components/ui/button"
 import Link from 'next/link'
 import {t} from '@/scripts/translate'
 import { isExternalLink } from "@/lib/utils"
+import { useRouter } from "next/navigation"
+import {cn} from "@/lib/utils"
+
 
 interface MoreButtonProps {
     label: string;
@@ -12,6 +17,17 @@ interface MoreButtonProps {
 }
 
 export function MoreButton({label, href, className, variant="default", size="default"}: MoreButtonProps){
+
+    const {push} = useRouter()
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        if (isExternalLink(href)) {
+            window.open(href, '_blank')
+        } else {
+            push(href)
+        }
+    }
     return isExternalLink(href) ? (
         <Button className={className} asChild variant={variant} size={size}>
         <a href={href} target="_blank" rel="noopener noreferrer">
@@ -19,8 +35,8 @@ export function MoreButton({label, href, className, variant="default", size="def
         </a>
         </Button>
     ) : (
-        <Button className={className} asChild variant={variant} size={size}>
-        <Link href={href}>{t(label)}</Link>
+        <Button onClick={handleClick} className={cn(className, "cursor-pointer")} variant={variant} size={size}>
+        {t(label)}
         </Button>
     )
 }
