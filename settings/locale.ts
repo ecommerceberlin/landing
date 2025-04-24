55e468c7c347dafc2c57f42aea4fa0841d2f996e
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-24 19:14:11 +0200

    revert primary phone prefixes to DE

--- settings/locale.ts
+++ settings/locale.ts
@@ -19,7 +19,7 @@ export function resolveAltLocale(){
 }
 
 
-export const primaryPrefixes = ["DE", "AT", "FR"] 
+export const primaryPrefixes = ["DE"] 
 
 
 export const countryPrefixes: CountryPrefixes = {
